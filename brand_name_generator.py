876ae337a2af47e7309fb3dec4ce2728d879a9b2
Vidyaa Ganesh <ganesh.srividyaa@gmail.com>
2023-08-04 14:05:31 -0400

    added comments

--- brand_name_generator.py
+++ brand_name_generator.py
@@ -1,3 +1,5 @@
+#brand name generator
+#work done for 100 days of python coursework on udemy
 print("Welcome to Your Brand Name Generator v1.0\n")
 city = (input("Which city did you grow up in?\n"))
 pet = (input("What is the name of your pet?\n"))
